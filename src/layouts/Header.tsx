77c8857b37d33604be5e5522298d0b8be699ac29
fGiordi <giordifungula@gmail.com>
2023-11-02 02:44:02 +0200

    updated class name

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -56,14 +56,11 @@ function Header({ openSidebar }: IHeader) {
 				<option value="move">Campaign Move</option>
 				<option value="shooters">Campaign Shooters</option>
 			</select>
-
-			
-
 			
 			</div>
 
-			<div>
-				<select  value={selectedSalesOption} onChange={handleNumSales} id="sales" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+			<div className='header-center'>
+				<select value={selectedSalesOption} onChange={handleNumSales} id="sales" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 				<option selected disabled>Product Sales filter</option>
 				<option value="highest">Highest Month for Product Sales</option>
 				<option value="lowest">Lowest Month for Product Sales</option>
