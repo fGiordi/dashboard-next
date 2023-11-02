f554d538c914b6e7c3343138c69a839f7a0832dd
fGiordi <giordifungula@gmail.com>
2023-11-02 02:51:20 +0200

    updated background color in dropdown

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -62,8 +62,8 @@ function Header({ openSidebar }: IHeader) {
 			<div className='header-center'>
 				<select value={selectedSalesOption} onChange={handleNumSales} id="sales" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 				<option selected disabled>Product Sales filter</option>
-				<option value="highest">Highest Month for Product Sales</option>
-				<option value="lowest">Lowest Month for Product Sales</option>
+				<option value="highest" className='bg-green-500 text-white'>Highest Month for Product Sales</option>
+				<option value="lowest" className='bg-red-500 text-white'>Lowest Month for Product Sales</option>
 			</select>
 			</div>
 			
